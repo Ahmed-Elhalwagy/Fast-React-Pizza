143d59c2690fd68d69aa3348ce51f7a4fe803617
Ahmed-Elhalwagy <ahmed.elhalwagy88@gmail.com>
2023-08-04 03:32:09 +0300

    intial commit

---
+++ src/ui/Header.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Link } from "react-router-dom";
+
+function Header() {
+  return (
+    <>
+      <Link to="/">Fast React Pizza Co.</Link>
+    </>
+  );
+}
+
+export default Header;
