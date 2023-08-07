b470dc6e1fc630be19b3683948a9e3b37406334e
Ahmed-Elhalwagy <ahmed.elhalwagy88@gmail.com>
2023-08-07 06:34:16 +0300

    add styles with Tailwind

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,21 +1,23 @@
-import React from "react";
-import Header from "./Header";
-import CartOverview from "../features/cart/CartOverview";
-import { Outlet, useNavigation } from "react-router-dom";
-import Loader from "../ui/Loader";
-import SearchOrder from "../features/order/SearchOrder";
+import Header from './Header';
+import Loader from './Loader';
+import CartOverview from '../features/cart/CartOverview';
+import { Outlet, useNavigation } from 'react-router-dom';
 
 function AppLayout() {
   const navigation = useNavigation();
-  const isLoading = navigation.state === "loading";
+  const isLoading = navigation.state === 'loading';
+
   return (
-    <div className="layout">
-      <Header />
-      <SearchOrder />
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {isLoading && <Loader />}
-      <main>
-        <Outlet />
-      </main>
+
+      <Header />
+
+      <div className="overflow-scroll">
+        <main className="mx-auto max-w-3xl">
+          <Outlet />
+        </main>
+      </div>
 
       <CartOverview />
     </div>
